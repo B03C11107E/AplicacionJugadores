d6fb63c395c60407be9c2b2f4dd054cff17d1700
B03C11107E <samueldesarrolloappsweb@gmail.com>
2023-11-26 23:54:31 +0000

    Commit que faltó

--- app/src/main/java/com/example/aplicacionjugadores/ui/pantallas/jugadores.kt
+++ app/src/main/java/com/example/aplicacionjugadores/ui/pantallas/jugadores.kt
@@ -1,10 +1,12 @@
 package com.example.aplicacionjugadores.ui.pantallas
 
+import android.net.Uri
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.focusable
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -13,11 +15,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Delete
@@ -35,47 +35,55 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusTarget
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.aplicacionjugadores.R
-import com.example.aplicacionjugadores.jugador
+import com.example.aplicacionjugadores.partidaJugador
+import com.example.aplicacionjugadores.toJson
 import com.example.aplicacionjugadores.ui.rutas.rutas
+import guardarXMLjugadores
+import leerXMLjugadores
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-var listaJugadores: ArrayList<jugador> = arrayListOf(
-    jugador("jugador 1", "equipo1"),
-    jugador("jugador2", "equipo2")
-)
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun PantallaHome(navController: NavHostController?) {
-
+    var listaJugadores: ArrayList<partidaJugador> = leerXMLjugadores()
     var deleteMode by remember { mutableStateOf(false) }
     var searchText by remember { mutableStateOf("") }
-    var listaMutable = remember { mutableStateListOf<jugador>().apply { addAll(listaJugadores) } }
-    var selectedOptions by remember { mutableStateOf(listOf<jugador>()) }
-    val scrollState = rememberScrollState()
+    var listaMutable = remember { mutableStateListOf<partidaJugador>().apply { addAll(listaJugadores) } }
+    guardarXMLjugadores(listaMutable)
+    var selectedOptions by remember { mutableStateOf(listOf<partidaJugador>()) }
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
         Text("Bienvenido", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
         TextField(
             value = searchText,
             onValueChange = { searchText = it },
             label = { Text("Search") },
             leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         )
-        LazyColumn(contentPadding = PaddingValues(8.dp), modifier = Modifier.fillMaxSize()) {
-            items(listaJugadores) { jugador->
+        LazyColumn(contentPadding = PaddingValues(8.dp)) {
+            items(listaJugadores) { jugador ->
                 if (searchText == "") {
-                    Carta(R.drawable.ic_launcher_background, jugador, funcion = @Composable {
+                    Carta( jugador,navController, funcion = @Composable {
                         if (deleteMode) {
                             Checkbox(
                                 checked = selectedOptions.contains(jugador),
@@ -96,14 +104,16 @@ fun PantallaHome(navController: NavHostController?) {
                         if (!deleteMode) {
                             if (selectedOptions.isNotEmpty()) {
                                 listaMutable.removeAll(selectedOptions.toSet())
+                                listaJugadores.removeAll(selectedOptions.toSet())
+                                guardarXMLjugadores(listaMutable)
+                                selectedOptions = emptyList()
                             }
                         }
                     })
-                }
-                else{
-                    if (jugador.equipo.contains(searchText)) {
-                        Carta(R.drawable.ic_launcher_background,
-                            jugador, funcion = @Composable {
+                } else {
+                    if (jugador.equipo.toLowerCase().contains(searchText.toLowerCase())) {
+                        Carta(
+                            jugador, navController, funcion = @Composable {
                                 if (deleteMode) {
                                     Checkbox(
                                         checked = selectedOptions.contains(jugador),
@@ -124,20 +134,31 @@ fun PantallaHome(navController: NavHostController?) {
                                 if (!deleteMode) {
                                     if (selectedOptions.isNotEmpty()) {
                                         listaMutable.removeAll(selectedOptions.toSet())
+                                        listaJugadores.removeAll(selectedOptions.toSet())
+                                        guardarXMLjugadores(listaJugadores)
+                                        selectedOptions = emptyList()
                                     }
                                 }
                             })
                     }
                 }
-
             }
         }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.BottomCenter),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             ExtendedFloatingActionButton(
-                onClick = { navController?.navigate(rutas.crear.ruta) },
+                onClick = {navController?.navigate(rutas.crear.ruta) },
                 icon = { Icon(Icons.Filled.AddCircle, "Extended floating action button.") },
                 text = { Text(text = "Add") },
                 modifier = Modifier.padding(16.dp)
@@ -151,27 +172,28 @@ fun PantallaHome(navController: NavHostController?) {
         }
     }
 }
-
 @Composable
-fun Carta(imagen: Int, jugador: jugador, funcion: (@Composable () -> Unit)) {
+fun Carta(jugador: partidaJugador,navController: NavHostController?, funcion: (@Composable () -> Unit)) {
 
-    Card(modifier = Modifier.fillMaxWidth(), border = BorderStroke(2.dp, Color.Black)) {
+    Card(modifier = Modifier.fillMaxWidth()
+        .clickable {
+            navController?.navigate("${rutas.detalles.ruta}/${jugador.nombre}")
+        }
+        , border = BorderStroke(2.dp, Color.Black)) {
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            Image(
-                painter = painterResource(imagen), contentDescription = "",
-                contentScale = ContentScale.Crop, modifier = Modifier
-                    .size(64.dp)
-                    .clip(
-                        CircleShape
-                    )
-            )
+            val formatoFecha = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val fechaFormateada = formatoFecha.format(jugador.fecha)
             Column(modifier = Modifier.weight(5F)) {
                 Text(
-                    text = jugador.nombre, textAlign = TextAlign.Center, modifier = Modifier
+                    text = jugador.nombre + " | "+jugador.equipo+ " VS " + jugador.contra, textAlign = TextAlign.Center, modifier = Modifier
                         .fillMaxWidth()
                 )
                 Text(
-                    text = jugador.equipo, textAlign = TextAlign.Center, modifier = Modifier
+                    text = jugador.juego + " "+ jugador.personaje, textAlign = TextAlign.Center, modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Text(
+                    text = fechaFormateada  + " "+ jugador.score, textAlign = TextAlign.Center, modifier = Modifier
                         .fillMaxWidth()
                 )
             }
